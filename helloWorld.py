15a1f174adf24a568d5d25889d51abbaee246b48
Lendl Munoz <lendlmunoz@gmail.com>
2024-03-27 21:24:32 +0000

    added a second input

--- helloWorld.py
+++ helloWorld.py
@@ -1,2 +1,3 @@
 var = input("Say something: ")  # ask user to type something to be printed.
-print(var)
+var2 = input("Say something else: ")  # ask the user to type something else to be printed as well.
+print(var + " and " + var2)
